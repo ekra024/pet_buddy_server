0adc5a277a3966fb8a5f1f0a3d6a9399c4857454
ekra024 <ekra.cse8.bu@gmail.com>
2026-01-11 23:21:12 +0600

    verifyAdmin, allPets allCampaigns created

--- index.js
+++ index.js
@@ -2,13 +2,37 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const admin = require("firebase-admin");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const serviceAccount = require("./firebase-admin-key.json");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const verifyUser = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+    const token = authHeader.split(" ")[1];
+
+    const decodedUser = await admin.auth().verifyIdToken(token);
+    req.decoded = decodedUser;
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "Invalid token" });
+  }
+};
+
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ps6f07s.mongodb.net/?appName=Cluster0`;
@@ -32,13 +56,27 @@ async function run() {
     const petsCollection = db.collection("pets");
     const campaignsCollection = db.collection("campaign");
     const adoptionCollection = db.collection("adoptionPets");
-    const donationsCollection = db.collection('donations');
+    const donationsCollection = db.collection("donations");
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+
+      const user = await usersCollection.findOne({ email });
+      
+
+      if (!user || user.role != "admin") {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+      next();
+    };
+    module.exports = verifyAdmin;
 
     app.post("/users", async (req, res) => {
       const email = req.body.email;
       const userExists = await usersCollection.findOne({ email });
       if (userExists) {
         return res
+          .updateOne({})
           .status(200)
           .send({ message: "User already exists", inserted: false });
       }
@@ -77,21 +115,21 @@ async function run() {
       }
     });
 
-    app.put('/pets/:id', async(req, res) => {
-      try{
+    app.put("/pets/:id", async (req, res) => {
+      try {
         const id = req.params.id;
         const updatedPet = req.body;
-        console.log(id);
+        
         const result = await petsCollection.updateOne(
-          {_id: new ObjectId(id)},
-          {$set: updatedPet}
+          { _id: new ObjectId(id) },
+          { $set: updatedPet }
         );
-        console.log(result);
+       
         res.send(result);
-      }catch(err){
-        res.status(500).json({error: "Internal Server Error"});
+      } catch (err) {
+        res.status(500).json({ error: "Internal Server Error" });
       }
-    })
+    });
 
     app.get("/pets/available", async (req, res) => {
       try {
@@ -123,103 +161,110 @@ async function run() {
       }
     });
 
-    app.patch('/pets/adopt/:id', async(req, res) => {
-      console.log('hit here adopt');
-      try{
+    app.patch("/pets/adopt/:id", async (req, res) => {
+      try {
         const id = req.params.id;
-      
-        console.log(id);
 
         const result = await petsCollection.updateOne(
-          {_id: new ObjectId(id)},
-          {$set: {adoption: true}},
-        )
+          { _id: new ObjectId(id) },
+          { $set: { adoption: true } }
+        );
         res.send(result);
-      }catch(err) {
-        res.status(500).json({error: "Internal Server Error"});
+      } catch (err) {
+        res.status(500).json({ error: "Internal Server Error" });
       }
-    })
+    });
 
     app.get("/pets/:id", async (req, res) => {
       try {
         const id = req.params.id;
         const pet = await petsCollection.findOne({ _id: new ObjectId(id) });
 
-        
-
         if (!pet) {
           return res.status(404).json({ message: "Pet not found" });
         }
-        
-        
+
         res.send(pet);
       } catch (err) {
         res.status(500).json({ message: "Server error" });
       }
     });
 
-    app.get("/pets/user/:email", async (req, res) => {
+    app.get("/pets/user/:email", verifyUser, async (req, res) => {
       const email = req.params.email;
-     
+      const userEmail = req.decoded.email;
+      if (email != userEmail) {
+        res.status(403).json({ message: "Unauthorized Access" });
+      }
       try {
         const result = await petsCollection.find({ email: email }).toArray();
-       
+
         res.send(result);
       } catch (err) {
         res.status(404).json({ error: "Data is not Found" });
       }
     });
 
-    app.post('/pet/adoptions',async(req, res) => {
-      try{
+    app.post("/pet/adoptions", async (req, res) => {
+      try {
         const adoption = {
           ...req.body,
-          status:"pending",
+          status: "pending",
           createdAt: new Date(),
         };
 
         const result = await adoptionCollection.insertOne(adoption);
         res.send(result);
-
-      }catch(err){
-        res.status(500).json({error: "Failed to submit adoption request"});
+      } catch (err) {
+        res.status(500).json({ error: "Failed to submit adoption request" });
       }
-    })
+    });
 
-    app.patch('/pet/adoptions/:id',async(req, res) => {
-      try{
+    app.patch("/pet/adoptions/:id", async (req, res) => {
+      try {
         const id = req.params.id;
-        const {status} = req.body;
-        const result = await adoptionCollection.updateOne(
-          {_id: new ObjectId(id)},
-          {$set: {status: status}}
+        const { status } = req.body;
+        const { petId } = req.body;
+
+        await adoptionCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: { status: status } }
         );
-        res.send(result);
-      }catch(err) {
-        res.status(500).json({error: "Internal Server Error"});
+        if (status === "accepted") {
+          await petsCollection.updateOne(
+            { _id: new ObjectId(petId) },
+            { $set: { adoption: true } }
+          );
+        }
+        res.send({ message: "Update complete" });
+      } catch (err) {
+        res.status(500).json({ error: "Internal Server Error" });
       }
-    })
+    });
 
-    app.get('/pet/adoptions/user/:email',async(req, res) => {
-      try{
-        const email = req.params.email;
-        console.log(email);
-        
-        const result = await adoptionCollection.find({
-          ownerEmail: email
-        }).toArray();
-        
-        res.send(result);
-      }catch(err){
-        res.status(500).json({error: "Internal Server Error"});
+    app.get("/pet/adoptions/user/:email", verifyUser, async (req, res) => {
+      const email = req.params.email;
+      const userEmail = req.decoded.email;
+      if (email != userEmail) {
+        res.status(403).json({ message: "Unauthorized Access" });
       }
-    })
+      try {
+        const result = await adoptionCollection
+          .find({
+            ownerEmail: email,
+          })
+          .toArray();
 
+        res.send(result);
+      } catch (err) {
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
 
     app.post("/campaigns", async (req, res) => {
       try {
         const camp = req.body;
-        
+
         const result = await campaignsCollection.insertOne(camp);
         res.send(result);
       } catch (err) {
@@ -227,29 +272,33 @@ async function run() {
       }
     });
 
-    app.get('/campaigns/available',async(req, res) => {
-      try{
-        const result = await campaignsCollection.find().sort({date:-1}).toArray();
+    app.get("/campaigns/available", async (req, res) => {
+      try {
+        const result = await campaignsCollection
+          .find()
+          .sort({ date: -1 })
+          .toArray();
 
         res.send(result);
-
-      }catch(err){
-        res.status(404).json({message:"Data is not Found"})
+      } catch (err) {
+        res.status(404).json({ message: "Data is not Found" });
       }
-    })
+    });
 
-    app.get('/campaigns/:id', async(req, res) =>{
-      try{
+    app.get("/campaigns/:id", async (req, res) => {
+      try {
         const id = req.params.id;
-        const result = await campaignsCollection.findOne({_id: new ObjectId(id) });
+        const result = await campaignsCollection.findOne({
+          _id: new ObjectId(id),
+        });
         res.send(result);
-      }catch(err) {
-        res.status(500).send({message:"Internal Server Error"})
+      } catch (err) {
+        res.status(500).send({ message: "Internal Server Error" });
       }
-    })
+    });
 
-    app.put('/campaigns/:id', async(req, res) => {
-      try{
+    app.put("/campaigns/:id", async (req, res) => {
+      try {
         const id = req.params.id;
         const updatedData = req.body;
 
@@ -262,12 +311,14 @@ async function run() {
       } catch (err) {
         res.status(500).send({ message: "Internal Server Error" });
       }
-    })
+    });
 
-    app.get("/campaigns/user/:email", async (req, res) => {
-      
+    app.get("/campaigns/user/:email", verifyUser, async (req, res) => {
       const email = req.params.email;
-   
+      const userEmail = req.decoded.email;
+      if (email != userEmail) {
+        res.status(403).json({ message: "Unauthorized Access" });
+      }
       try {
         const result = await campaignsCollection
           .find({ userEmail: email })
@@ -279,103 +330,213 @@ async function run() {
       }
     });
 
-    app.delete('/campaigns/:id', async(req, res) => {
-      try{
-        const id = req.params.id; 
-        const result = await campaignsCollection.deleteOne({_id: new ObjectId(id)});
-        res.send(result);
-      }catch(err){
-        res.status(500).send({message:"Internal Server Error"})
-      }
-    })
-
-    app.patch('/campaigns/donate/:id', async(req, res) => {
-      try{
+    app.delete("/campaigns/:id", async (req, res) => {
+      try {
         const id = req.params.id;
-        const { amount} = req.body;
+        const result = await campaignsCollection.deleteOne({
+          _id: new ObjectId(id),
+        });
+        res.send(result);
+      } catch (err) {
+        res.status(500).send({ message: "Internal Server Error" });
+      }
+    });
+
+    app.patch("/campaigns/donate/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const { amount } = req.body;
         const result = await campaignsCollection.updateOne(
-          {_id: new ObjectId(id)},
-          {$inc: {donatedAmount: amount}}
+          { _id: new ObjectId(id) },
+          { $inc: { donatedAmount: amount } }
         );
         res.send(result);
-      }catch(err){
-        res.status(500).send({message:"Internal Server Error"});      
+      } catch (err) {
+        res.status(500).send({ message: "Internal Server Error" });
       }
-    })
+    });
 
-    app.post('/donations',async (req, res) => {
+    app.post("/donations", async (req, res) => {
       const donation = {
         ...req.body,
-        createdAt : new Date(),
+        createdAt: new Date(),
       };
       const donationAmount = Number(donation.amount);
-      if(isNaN(donationAmount) || donationAmount <= 0) {
-        return res.status(400).send({message: 'Invalid donation amount'});
+      if (isNaN(donationAmount) || donationAmount <= 0) {
+        return res.status(400).send({ message: "Invalid donation amount" });
       }
       const result = await donationsCollection.insertOne(donation);
-      
+
       const updateResult = await campaignsCollection.updateOne(
-        {_id: new ObjectId(donation.campaignId)},
-        {$inc: {donatedAmount: donationAmount}}
-      )
-      
-      if(updateResult.modifiedCount === 0) {
-        return res.status(404).send({message:'Campaign not found'});
+        { _id: new ObjectId(donation.campaignId) },
+        { $inc: { donatedAmount: donationAmount } }
+      );
+
+      if (updateResult.modifiedCount === 0) {
+        return res.status(404).send({ message: "Campaign not found" });
       }
       res.send(result);
-      
-    })
+    });
 
-    app.get('/donations/campaign/:id',async(req, res) => {
-      try{
+    app.get("/donations/campaign/:id", async (req, res) => {
+      try {
         const campaignId = req.params.id;
-        const result = await donationsCollection.find({campaignId: campaignId}).toArray();
-        console.log(result);
+        const result = await donationsCollection
+          .find({ campaignId: campaignId })
+          .toArray();
+        
         res.send(result);
-      }catch(err){
-        res.status(500).send({message:"Internal Server Error"});
+      } catch (err) {
+        res.status(500).send({ message: "Internal Server Error" });
       }
-    })
+    });
 
-    app.patch('/campaigns/pause/:id',async(req, res) => {
-      try{
+    app.patch("/campaigns/pause/:id", async (req, res) => {
+      try {
         const id = req.params.id;
-        const {paused} = req.body;
+        const { paused } = req.body;
         const result = await campaignsCollection.updateOne(
-          {_id: new ObjectId(id)},
-          {$set: {paused: paused}}
+          { _id: new ObjectId(id) },
+          { $set: { paused: paused } }
         );
         res.send(result);
-      }catch(err){
-        res.status(500).send({message:"Internal Server Error"});
-
+      } catch (err) {
+        res.status(500).send({ message: "Internal Server Error" });
       }
-    })
+    });
 
-    app.get('/donations/user/:email',async(req, res) => {
-      try{
-        const email = req.params.email;
-        const result = await donationsCollection.find({donorEmail: email}).toArray();
+    app.get("/donations/user/:email", verifyUser, async (req, res) => {
+      const email = req.params.email;
+      const userEmail = req.decoded.email;
+      if (email != userEmail) {
+        res.status(403).json({ message: "Unauthorized Access" });
+      }
+      try {
+        const result = await donationsCollection
+          .find({ donorEmail: email })
+          .toArray();
         res.send(result);
-      }catch(err){
-        res.status(500).send({message:"Internal Server Error"});
+      } catch (err) {
+        res.status(500).send({ message: "Internal Server Error" });
       }
-    })
-    
+    });
 
-    app.post('/create-payment-intent',async(req, res) => {
-      const {amount} = req.body;
+    app.post("/create-payment-intent", async (req, res) => {
+      const { amount } = req.body;
 
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount * 100,
-        currency: 'usd',
-        payment_method_types: ['card'],
+        currency: "usd",
+        payment_method_types: ["card"],
       });
 
       res.send({
         clientSecret: paymentIntent.client_secret,
       });
+    });
 
+    app.get(
+      "/all-donations/admin/:email",
+      verifyUser,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ admin: false });
+        }
+        try {
+          const result = await donationsCollection.find().toArray();
+          res.send(result);
+        } catch (err) {
+          res.status(500).json({ message: "Internal Server Error" });
+        }
+      }
+    );
+
+    app.get(
+      "/users/admin/:email",
+      verifyUser,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ admin: false });
+        }
+
+        const user = await usersCollection.findOne({ email });
+        res.send({ admin: user?.role === "admin" });
+      }
+    );
+
+    app.get(
+      "/allPets/admin/:email",
+      verifyUser,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        if (email != req.decoded.email) {
+          res.status(403).json({message:"unauthorized acess"})
+        }
+        try{
+          const result = await petsCollection.find().toArray();
+          res.send(result);
+        }catch(err){
+          res.status(500).json({message:"Internal Server Error"})
+        }
+      }
+    );
+
+    app.get(
+      "/allCampaigns/admin/:email",
+      verifyUser,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        if (email != req.decoded.email) {
+          res.status(403).json({message:"unauthorized acess"})
+        }
+        try{
+          const result = await campaignsCollection.find().toArray();
+          res.send(result);
+        }catch(err){
+          res.status(500).json({message:"Internal Server Error"})
+        }
+      }
+    );
+    app.get(
+      "/allDonations/admin/:email",
+      verifyUser,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        if (email != req.decoded.email) {
+          res.status(403).json({message:"unauthorized acess"})
+        }
+        try{
+          const result = await donationsCollection.find().toArray();
+          res.send(result);
+        }catch(err){
+          res.status(500).json({message:"Internal Server Error"})
+        }
+      }
+    );
+
+    app.patch('/admin/campaigns/:id/status', async(req, res) => {
+      try{
+        console.log('hit admin camp')
+        const id = req.params.id;
+        const result = await campaignsCollection.updateOne(
+          {_id: new Object(id)},
+          {$set:{
+            paused: false,
+          }}
+        )
+        console.log(result);
+        res.send(result);
+      }catch(err){
+         res.status(500).json({message:"Internal Server Error"})
+      }
     })
 
     // Send a ping to confirm a successful connection
